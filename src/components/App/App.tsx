3893e3e36cb39efe3d4e226967b0a9897e150548
Alexander Pashkov <alexpashkov123@gmail.com>
2018-10-20 12:27:48 +0300

    Restructure default file structure.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Strip from "../Strip/Strip";
+import "./App.css";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Strip />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Strip/Strip.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+
+
+const Strip: React.StatelessComponent<{}> = () => <div>Hi</div>;
+
+export default Strip;
